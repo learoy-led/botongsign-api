4a1cf2b3516c906bf192043d649012595b728d56
learoy-led <marketing@learoyled.com>
2024-10-22 15:11:04 +0200

    place dotenv import first

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+
 
 const app = express();
 
